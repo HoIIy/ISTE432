7bd50f809b4c318c08377206c74408ddb8fb808a
Ian Kitchen <igk2718@rit.edu>
2017-11-02 23:55:04 -0400

    Changed API call
    API call to get stations now only grabs for NY. This returns a much
    smaller data set

--- app/api/ApiGateway.php
+++ app/api/ApiGateway.php
@@ -6,7 +6,7 @@ if(isset($_POST))
     {
         case 'get stations':
             // Grab data from the API and parse it into an assoc array
-            $fuelStations = file_get_contents("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=tx9yueaUYcSYJn46Jov6S2KaP0F6h2oeWpgaPM9c&format=JSON");
+            $fuelStations = json_decode(file_get_contents("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=tx9yueaUYcSYJn46Jov6S2KaP0F6h2oeWpgaPM9c&format=JSON&status=E&state=NY"), true);
 
             if(isset($fuelStations['error']))
             {
@@ -18,7 +18,7 @@ if(isset($_POST))
             {
                 // Echo the data back to the js
                 // Once the database is designed, we can call another script to load this data into the DB
-                echo $fuelStations;
+                echo var_dump($fuelStations);
                 die;
             }
             break;
