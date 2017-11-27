44b398fa81930911118ae9e725b6faea73765e77
Holly <hmb1407@g.rit.edu>
2017-11-27 13:30:49 -0500

    holly

--- app/index.php
+++ app/index.php
@@ -32,6 +32,39 @@
         <div class="w3-row-padding">
 
         <!-- Left Column -->
+        <div class="w3-twothird">
+
+          <!-- Google Map -->
+          <div class="w3-container w3-card w3-white w3-margin-bottom">
+            <div id="map" class="w3-container"></div>
+          </div>
+		  
+		  <!-- Content -->
+          <div class="w3-container w3-card w3-white">
+            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>LIST</h2>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Station 1</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
+              <p>Web Development! All I need to know in one place</p>
+              <hr>
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Station 2</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
+              <p>Master Degree</p>
+              <hr>
+            </div>
+            <div class="w3-container">
+              <h5 class="w3-opacity"><b>Station 3</b></h5>
+              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
+              <p>Bachelor Degree</p><br>
+            </div>
+          </div>
+
+        <!-- End Left Column -->
+        </div>
+
+        <!-- Right Column -->
         <div class="w3-third">
 
             <div class="w3-white w3-text-grey w3-card-4">
@@ -135,38 +168,6 @@
 
                 </form>
             </div>
-
-        <!-- End Left Column -->
-        </div>
-
-        <!-- Right Column -->
-        <div class="w3-twothird">
-
-          <!-- Google Map -->
-          <div class="w3-container w3-card w3-white w3-margin-bottom">
-            <div id="map" class="w3-container"></div>
-          </div>
-
-          <div class="w3-container w3-card w3-white">
-            <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>LIST</h2>
-            <div class="w3-container">
-              <h5 class="w3-opacity"><b>Station 1</b></h5>
-              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
-              <p>Web Development! All I need to know in one place</p>
-              <hr>
-            </div>
-            <div class="w3-container">
-              <h5 class="w3-opacity"><b>Station 2</b></h5>
-              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
-              <p>Master Degree</p>
-              <hr>
-            </div>
-            <div class="w3-container">
-              <h5 class="w3-opacity"><b>Station 3</b></h5>
-              <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
-              <p>Bachelor Degree</p><br>
-            </div>
-          </div>
 
         <!-- End Right Column -->
         </div>
